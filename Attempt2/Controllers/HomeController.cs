868931ec9ba325b11d062cd8835deca6b28c0dcb
walkerdarrion1@gmail.com <walkerdarrion1@gmail.com>
2020-04-20 15:15:44 -0400

    Sync with Main Project

--- Attempt2/Controllers/HomeController.cs
+++ Attempt2/Controllers/HomeController.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Attempt2.Models;
+using System.Data.SqlClient;
 
 namespace Attempt2.Controllers
 {
+
+    // attempting changes on local clone
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+       // private string dbConnection = "Database=master;Server=LAPTOP-B57TLJ6S\\SCHEDULER;" +
+         //   "Integrated Security=True;connect timeout = 60";
+         private string dbConnection = "Database=master;Server=DESKTOP-KKOGS2A\\SQLEXPRESS;" +
+           "Integrated Security=True;connect timeout = 60";
+        List<string> times = new List<string>();
+        List<string> days = new List<string>();
+
+        //adding to test branch use
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -22,17 +31,13 @@ namespace Attempt2.Controllers
         {
             return View();
         }
-        public IActionResult Page2(InfoModel im)
-
-           
+        public IActionResult Page2()
         {
-            return View(im);
+            return View();
         }
 
         public IActionResult UniqueCode()
         {
-
-
             return View();
         }
 
@@ -41,57 +46,114 @@ namespace Attempt2.Controllers
             return View();
         }
 
-        public IActionResult Page3()
+        // sending Creator Information to the database and returning unique code page
+        public IActionResult Page3(InfoModel im)
         {
-            LinkedList<InfoModel> dm = new LinkedList<InfoModel>();
-            InfoModel temp = new InfoModel();
-            temp.Day = "Sunday";
-            dm.AddFirst(temp);
+            // take unique code and connect to DB and check if it has been taken, if not add, if so try again
 
-            temp = new InfoModel();
-            temp.Day = "Saturday";
-            dm.AddFirst(temp);
+            // connect to the DB
+            SqlConnection conn = new SqlConnection(dbConnection);
+            conn.Open();
 
-            temp = new InfoModel();
-            temp.Day = "Friday";
-            dm.AddFirst(temp);
+            // create a command
+            string query = "INSERT INTO [dbo].[Creator_Info]([fname],[email],[reason]) VALUES (@fname, @email, @reason);";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@fname", im.FirstName);
+            cmd.Parameters.AddWithValue("@email", im.Email);
+            cmd.Parameters.AddWithValue("@reason", im.Desrcription);
 
-            temp = new InfoModel();
-            temp.Day = "Thursday";
-            dm.AddFirst(temp);
+            // query the DB
+            cmd.ExecuteNonQuery();
 
-            temp = new InfoModel();
-            temp.Day = "Wednesday";
-            dm.AddFirst(temp);
+            query = "INSERT INTO [dbo].[Users_Availability] VALUES(@uniquecode, (select userid from [dbo].[Creator_Info] where fname = @fname and email = @email and reason = @reason))";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@uniquecode", im.UniqueCode);
+            cmd.Parameters.AddWithValue("@fname", im.FirstName);
+            cmd.Parameters.AddWithValue("@email", im.Email);
+            cmd.Parameters.AddWithValue("@reason", im.Desrcription);
 
-            temp = new InfoModel();
-            temp.Day = "Tuesday";
-            dm.AddFirst(temp);
+            // query the DB
+            cmd.ExecuteNonQuery();
 
-            temp = new InfoModel();
-            temp.Day = "Monday";
-            dm.AddFirst(temp);
+            // close the database
+            conn.Close();
 
-            
+            RetrieveTimes();
 
-            ViewBag.LLDayModel = dm;
+            foreach (var item in times)
+            {
+                Console.WriteLine(item);
+            }
 
             return View();
         }
 
-        public IActionResult Page4(InfoModel DM)
+
+
+        private void RetrieveTimes()
+        {
+            SqlConnection conn = new SqlConnection(dbConnection);
+            conn.Open();
+
+            // create a command
+            string query = "SELECT [timeid],[time10] FROM [master].[dbo].[Time_10];";
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            SqlDataReader dataReader = cmd.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                var item = dataReader.GetValue(1);
+                Console.WriteLine(item);
+                Console.WriteLine(item.GetType());
+                times.Add(item.ToString());
+            }
+        }
+        // sending Creator Information to the database and returning unique code page
+        public IActionResult Page4(DaysChecked d)
+        {
+            // take unique code and connect to DB and check if it has been taken, if not add, if so try again
+            UserDaysChecked(d);
+
+            ViewBag.message = "Controller to View";
+
+            foreach (var item in d.daysAvaiable)
+            {
+                Console.WriteLine(item);
+            }
+            return View();
+        }
+
+        private void UserDaysChecked(DaysChecked d)
         {
 
-            
-
-            return View(DM);
+            if (d.monday)
+                days.Add("Monday");
+            if (d.tuesday)
+                days.Add("Tuesday");
+            if (d.wednesday)
+                days.Add("Wednesday");
+            if (d.thursday)
+                days.Add("Thursday");
+            if (d.friday)
+                days.Add("Friday");
+            if (d.saturday)
+                days.Add("Saturday");
+            if (d.sunday)
+                days.Add("Sunday");
         }
+
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private void DBConnect()
+        {
+
         }
     }
 }
---
+++ Attempt2/Models/DaysChecked.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Attempt2.Models
+{
+    public class DaysChecked
+    {
+        public bool monday { get; set; }
+        public bool tuesday { get; set; }
+        public bool wednesday { get; set; }
+        public bool thursday { get; set; }
+        public bool friday { get; set; }
+        public bool saturday { get; set; }
+        public bool sunday { get; set; }
+
+        public List<string> daysAvaiable = new List<string>();
+
+        public List<string> timesAvailable = new List<string>();
+
+    }
+}
